3ac1f73f14dd81d136ed01467fd36fefbc4e63b6
sparsh-anurag <sparsh.anurag@tothenew.com>
2023-02-13 16:17:50 +0530

    updated README.md and configuration files

--- example/terraform_ec2.tf
+++ example/terraform_ec2.tf
@@ -1,7 +1,8 @@
 
 module "ec2-terraform-agent" {
   source              = "git::https://github.com/tothenew/terraform-aws-agent.git"
-  key_name            = "dev-terraform-agent"
-  security_groups     = ["sg-999999999999"]
+  vpc_id              = "vpc-999999999999"
   subnet_id           = "subnet-999999999999"
 }
+
+
