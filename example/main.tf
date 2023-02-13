fac24d78fa27d02e6299b5fde74a5a8eaefe2018
sparsh-anurag <sparsh.anurag@tothenew.com>
2023-02-13 14:01:29 +0530

    Updated Terraform Agent Module

--- example/main.tf
+++ example/main.tf
@@ -1,7 +1,7 @@
 
 module "ec2-terraform-agent" {
-  source              = "../"
+  source              = "git::https://github.com/tothenew/terraform-aws-agent.git"
   key_name            = "tothenew"
-  security_groups     = ["sg-02e6ebd983b19c810"]
-  subnet_id           = "subnet-006ddef68deceb474"
+  security_groups     = ["sg-999999999999"]
+  subnet_id           = "subnet-999999999999"
 }
